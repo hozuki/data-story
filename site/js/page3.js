3b373a5da1a57463c1d704fdf68fefe49df1696f
George Wu <uiharu@buaa.edu.cn>
2016-12-07 15:41:13 +0100

    phase 4

--- site/js/page3.js
+++ site/js/page3.js
@@ -1,24 +1,33 @@
 "use strict";
 
 function main() {
-    var video = document.getElementById('video');
+    var video = document.getElementById("video");
+    var mediaConfig = {video: true};
+    var errBack = function (e) {
+        console.log("An error occurred.", e)
+    };
 
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({video: true})
-            .then(setupVideo);
-    } else if (navigator.getUserMedia) { // Standard
-        navigator.getUserMedia({video: true}, setupVideo, errBack);
-    } else if (navigator.webkitGetUserMedia) { // WebKit-prefixed
-        navigator.webkitGetUserMedia({video: true}, setupVideo, errBack);
-    } else if (navigator.mozGetUserMedia) { // Mozilla-prefixed
-        navigator.mozGetUserMedia({video: true}, setupVideo, errBack);
-    }
-
-    function setupVideo(stream) {
-        video.src = (window.webkitURL || window.URL).createObjectURL(stream);
-        video.play();
-
+        navigator.mediaDevices.getUserMedia(mediaConfig).then(function (stream) {
+            video.src = window.URL.createObjectURL(stream);
+            video.play();
+        });
+    } else if (navigator.getUserMedia) {
+        navigator.getUserMedia(mediaConfig, function (stream) {
+            video.src = stream;
+            video.play();
+        }, errBack);
+    } else if (navigator.webkitGetUserMedia) {
+        navigator.webkitGetUserMedia(mediaConfig, function (stream) {
+            video.src = window.webkitURL.createObjectURL(stream);
+            video.play();
+        }, errBack);
+    } else if (navigator.mozGetUserMedia) {
+        navigator.mozGetUserMedia(mediaConfig, function (stream) {
+            video.src = window.URL.createObjectURL(stream);
+            video.play();
+        }, errBack);
     }
 }
 
-main();
+document.body.onload = main;
