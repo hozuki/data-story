0cc3aa323ac6f23aad07b7db3e43b59877d5d6dc
George Wu <uiharu@buaa.edu.cn>
2016-12-13 14:08:06 +0100

    policy line

--- site/js/page1-line-chart.js
+++ site/js/page1-line-chart.js
@@ -59,13 +59,55 @@ class Page1LineChart {
         const yearStart = Page1LineChart.yearStart, yearEnd = Page1LineChart.yearEnd;
         const obesityDataSet = makeDataSet(this.data.worldObesity, country, yearStart, yearEnd);
         const smokerDataSet = makeDataSet(this.data.smokers, country, yearStart, yearEnd);
+        const policyYear = this.data.tobaccoPolicy[code].year;
 
         const width = div.clientWidth, height = div.clientHeight;
         const g = svg.append("g")
             .attr("transform", "translate(60,15)");
         const valueDomain = [0.1, 0.3];
+
+        const xScale = d3.scaleTime()
+            .rangeRound([0, width])
+            .domain([parseTime(yearStart - 5), parseTime(yearEnd + 5)]);
+        const yScale = d3.scaleLinear()
+            .rangeRound([height, 0])
+            .domain(valueDomain);
+
         makeChart(g, obesityDataSet, "d3-line-obesity", valueDomain);
         makeChart(g, smokerDataSet, "d3-line-smoker", valueDomain);
+        makePolicy(g, policyYear, valueDomain);
+
+        const xAxis = d3.axisBottom()
+            .scale(xScale)
+            .ticks(5);
+        const yAxis = d3.axisLeft()
+            .scale(yScale)
+            .ticks(5)
+            .tickFormat(d3.format("0.0%"));
+
+        g.append("g")
+            .attr("class", "x_axis")
+            .attr("transform", `translate(0,${height})`)
+            .call(xAxis);
+        g.append("g")
+            .attr("class", "y_axis")
+            .call(yAxis);
+
+        g.append("text")
+            .attr("text-anchor", "middle")
+            .attr("transform", `translate(${xScale(parseTime(policyYear))},${height + 35})`)
+            .text(policyYear)
+            .style("fill", "green");
+
+        g.append("text")
+            .attr("text-anchor", "middle")
+            .attr("transform", `translate(-40,${height / 2}) rotate(-90)`)
+            .text("Value");
+
+        g.append("text")
+            .attr("text-anchor", "middle")
+            .attr("transform", `translate(${width / 2},${height + 30})`)
+            .text("Year");
 
         function makeDataSet(massData, country, yearStart, yearEnd) {
             const list = [];
@@ -83,47 +125,34 @@ class Page1LineChart {
             return list;
         }
 
-        function makeChart(g, dataSet, className, domain) {
-            const xScale = d3.scaleTime()
-                .rangeRound([0, width])
-                .domain([parseTime(yearStart - 5), parseTime(yearEnd + 5)]);
-            const yScale = d3.scaleLinear()
-                .rangeRound([height, 0])
-                .domain(domain);
+        function makeChart(g, dataSet, className) {
             const lineData = d3.line()
                 .x(d => xScale(parseTime(d.year)))
                 .y(d => yScale(d.value / 100));
 
-            const xAxis = d3.axisBottom()
-                .scale(xScale)
-                .ticks(5);
-            const yAxis = d3.axisLeft()
-                .scale(yScale)
-                .ticks(5)
-                .tickFormat(d3.format("0.0%"));
             g.append("g")
                 .append("path")
                 .datum(dataSet)
                 .attr("class", className)
                 .attr("d", lineData);
-            g.append("g")
-                .attr("class", "x_axis")
-                .attr("transform", `translate(0,${height})`)
-                .call(xAxis);
-            g.append("g")
-                .attr("class", "y_axis")
-                .call(yAxis);
         }
 
-        g.append("text")
-            .attr("text-anchor", "middle")
-            .attr("transform", `translate(-40,${height / 2}) rotate(-90)`)
-            .text("Value");
+        function makePolicy(g, year, domain) {
+            console.log(year);
+            const policyDataSet = [
+                {year: year, value: domain[0] * 100},
+                {year: year, value: domain[1] * 100}
+            ];
+            const lineData = d3.line()
+                .x(d => xScale(parseTime(d.year)))
+                .y(d => yScale(d.value / 100));
 
-        g.append("text")
-            .attr("text-anchor", "middle")
-            .attr("transform", `translate(${width / 2},${height + 30})`)
-            .text("Year");
+            g.append("g")
+                .append("path")
+                .datum(policyDataSet)
+                .attr("class", "d3-line-policy")
+                .attr("d", lineData);
+        }
 
         function parseTime(year) {
             return new Date(Date.parse(String(year)));
@@ -161,7 +190,7 @@ class Page1LineChart {
      * @return {number}
      */
     static get yearStart() {
-        return 1995;
+        return 1990;
     }
 
     /**
